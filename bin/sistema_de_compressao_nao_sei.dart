32152a1365da21c39e6cd320896d9937d5f15f19
DIMAAGR <attackdiamond@gmail.com>
2021-12-15 16:25:33 -0300

    criado tela de boas vindas

--- bin/sistema_de_compressao_nao_sei.dart
+++ bin/sistema_de_compressao_nao_sei.dart
@@ -1,3 +1,16 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+void main() {
+  print('=======================================');
+  print('SISTEMA SIMPLES DE COMPRESSÃO: BY ITALO');
+  print('CRIADO EM 15/12/2021');
+  print('=======================================');
+
+  print('\nDigite o Local do Arquivo:');
+
+  String local = stdin.readLineSync()!;
+  File arquivo = File(local);
+  List<String> contents = arquivo.readAsLinesSync();
+
+  print('aguarde...');
 }
